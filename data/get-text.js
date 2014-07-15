1006c3c5fae0e1642a3abf78784f78eb47b0b3d6
blueish4 <michael@hathaways.me.uk>
2014-07-15 20:57:25 +0100

    Initial commit

---
+++ data/get-text.js
@@ -0,0 +1,30 @@
+// When the user hits return, send the "text-entered"
+// message to main.js.
+// The message payload is the contents of the edit box.
+var siteName = document.getElementById("site");
+var pword = document.getElementById("pword");
+siteName.addEventListener('keyup', function onkeyup(event) {
+  if (event.keyCode == 13) {
+    // Remove the newline.
+    text = [pword.value, siteName.value];
+    self.port.emit("text-entered", text);
+    siteName.value = '';
+  }
+}, false);
+pword.addEventListener('keyup', function onkeyup(event) {
+  if (event.keyCode == 13) {
+    // Remove the newline.
+    text = [pword.value, siteName.value];
+    self.port.emit("text-entered", text);
+    pword.value = '';
+  }
+}, false);
+// Listen for the "show" event being sent from the
+// main add-on code. It means that the panel's about
+// to be shown.
+//
+// Set the focus to the text area so the user can
+// just start typing.
+self.port.on("show", function onShow() {
+  siteName.focus();
+});
